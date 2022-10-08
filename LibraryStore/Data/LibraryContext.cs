e0d0ead9a37e81f66fd85331eeeb75ea62d8363e
seminire.adebiyi <seminire.adebiyi_kud@DESKTOP-R2FOFK1>
2022-10-08 12:24:50 +0100

    adding the dbcontexr

---
+++ LibraryStore/Data/LibraryContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LibraryStore.Data
+{
+    public class LibraryContext: DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options): base(options) 
+        {
+            //we have to pass the same settings in the base class constructor, which is DbContext, thats why we use the base keyword
+
+        }
+        public DbSet<Books> Books { get; set; }
+    }
+}
--- LibraryStore/Program.cs
+++ LibraryStore/Program.cs
@@ -1,4 +1,6 @@
+using LibraryStore.Data;
 using LibraryStore.Repository;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<ILibraryRepository, LibraryRepository>(); //registering the library service
+builder.Services.AddDbContext<LibraryContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
